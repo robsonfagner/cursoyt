a23fe355146ceeb2a44690e792c389ca55b32010
Robson Fagner <65050616+robsonfagner@users.noreply.github.com>
2023-12-21 21:48:48 -0300

    Aula 4 com scripts

--- Curso G.Guanabara/aula4.py
+++ Curso G.Guanabara/aula4.py
@@ -1,2 +1,10 @@
-print("Ola Mundo")
+#print("Ola Mundo")
 #print(9*8)
+
+#Utilizando Variavéis com letra minusculas, objeto é mais que variavel
+
+nome=input('Qual o seu nome: ')
+idade=input('Qual a sua idade: ')
+peso=input('Quanto você pesa? ')
+
+print(nome,idade,peso)
